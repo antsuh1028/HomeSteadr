8df0a232f9bec8f74c26cf50348ad10270ac870a
Jose <josegonz115@gmail.com>
2025-01-24 23:29:02 -0800

    configure firebase

---
+++ homesteadr-frontend/src/firebase/config.ts
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+};
+
+function validateFirebaseConfig(config: typeof firebaseConfig) {
+  for (const [key, value] of Object.entries(config)) {
+    if (!value) {
+      throw new Error(`Firebase config error: ${key} is not defined`);
+    }
+  }
+}
+validateFirebaseConfig(firebaseConfig);
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
